4f7fe4ddacc021b4738c360ed12b583cfdc33437
Your Name <166686180+allysonhd@users.noreply.github.com>
2024-07-24 18:44:15 -0500

    message tests complete

--- spec/message.spec.js
+++ spec/message.spec.js
@@ -1,9 +1,24 @@
-const Message = require('../message.js');
-const Command = require('../command.js');
+const Message = require("../message.js");
+const Command = require("../command.js");
 
 // NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
-describe("Message class", function() {
+describe("Message class", function () {
+  test("throws error if a name is NOT passed into the constructor as the first parameter", function () {
+    expect(function () {
+      new Message();
+    }).toThrow(new Error("Name required."));
+  });
 
+  test("constructor sets name", function () {
+    let testingClass = new Message("name");
+    expect(testingClass.name).toBe("name");
+  });
+
+  test("contains a commands array passed into the constructor as the 2nd argument", function () {
+    let testCommands = [new Command('MODE_CHANGE', 'value_One'), new Command('STATUS_CHECK'), new Command('MOVE', 'value_Two')];
+    let testingClass = new Message("name", testCommands);
+    expect(testingClass.commands).toBe(testCommands);
+  });
 });
